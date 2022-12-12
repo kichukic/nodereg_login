0dbfd76fe54d0967dbda77506f4295cdc43ec8f5
HIJACK <38286414+kichukic@users.noreply.github.com>
2022-12-12 05:28:55 +0000

    new code

--- src/mongodb.js
+++ src/mongodb.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb://localhost:27017/TEST").then(()=>{
+mongoose.connect("mongodb://127.0.0.1:27017/TEST").then(()=>{
     console.log("Connected to database")
 }).catch(()=>{
     console.log("Failed to connect to database")
